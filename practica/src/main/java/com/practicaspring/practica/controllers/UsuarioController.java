de29f2922303aec38550ec61adee9ce907469895
jeiTamara <ing.jeisontamara@gmail.com>
2023-07-01 01:03:10 -0500

    agregamos lista (usuarios)
    agregamos requerimientos para mostrar tabla de usuarios al iniciar la pagina

--- practica/src/main/java/com/practicaspring/practica/controllers/UsuarioController.java
+++ practica/src/main/java/com/practicaspring/practica/controllers/UsuarioController.java
@@ -5,6 +5,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 
 /*Los controladores sirven para manejar las url */
@@ -22,4 +25,33 @@ public class UsuarioController {
         return usuario;
     }
 
+    @RequestMapping(value= "usuarios")//
+    public List<Ususario> getUsuarios() {
+        List<Ususario> usuarios = new ArrayList<>();
+        Ususario usuario = new Ususario();
+        usuario.setId(23);
+        usuario.setNombre("jeison");
+        usuario.setApellido("tamara");
+        usuario.setTelefono("748399378");
+        usuario.setEmail("hdhdhd@jfjfhf.com");
+
+        Ususario usuario2 = new Ususario();
+        usuario2.setId(98);
+        usuario2.setNombre("andres");
+        usuario2.setApellido("sangregorio");
+        usuario2.setTelefono("273618273");
+        usuario2.setEmail("andres@hotmail.com");
+
+        Ususario usuario3 = new Ususario();
+        usuario3.setId(198);
+        usuario3.setNombre("luis");
+        usuario3.setApellido("marcel");
+        usuario3.setTelefono("290484720");
+        usuario3.setEmail("loce@jmail.com");
+
+        usuarios.add(usuario);
+        usuarios.add(usuario2);
+        usuarios.add(usuario3);
+        return usuarios;
+    }
 }
