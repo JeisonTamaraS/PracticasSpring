d469424d9da3eaadd6aa1b17df0c9ef625358018
jeiTamara <ing.jeisontamara@gmail.com>
2023-08-25 12:21:19 -0500

    implementación de controlador
    creamos el método controlador

--- Workspace/spring-boot-web/src/main/java/com/practica/springboot/web/app/controllers/IndexController.java
+++ Workspace/spring-boot-web/src/main/java/com/practica/springboot/web/app/controllers/IndexController.java
@@ -1,5 +1,13 @@
 package com.practica.springboot.web.app.controllers;
 
-public class IndexController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
+public class IndexController {
+	
+	@GetMapping(value={"/index","/home","/"})
+	public String index() {
+		return "index";
+	}
 }
