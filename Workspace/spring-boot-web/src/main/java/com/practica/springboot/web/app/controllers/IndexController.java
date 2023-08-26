4250c6c2ee050cfb8c44de15dcc56f2bef2c83b4
jeiTamara <ing.jeisontamara@gmail.com>
2023-08-26 18:40:09 -0500

    implementación básica de vista
    creamos un archivo .html el cual funciona como vista en el navegador donde se mostraran los datos

--- Workspace/spring-boot-web/src/main/java/com/practica/springboot/web/app/controllers/IndexController.java
+++ Workspace/spring-boot-web/src/main/java/com/practica/springboot/web/app/controllers/IndexController.java
@@ -1,13 +1,15 @@
 package com.practica.springboot.web.app.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
 	
 	@GetMapping(value={"/index","/home","/"})
-	public String index() {
+	public String index(Model modelo) {
+		modelo.addAttribute("Titulo", "Hola springboot framework");
 		return "index";
 	}
 }
