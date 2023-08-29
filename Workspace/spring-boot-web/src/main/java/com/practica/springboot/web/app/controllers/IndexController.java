ce61ab7f7daddaf2ee0b3f567cde6ae13661bc21
jeiTamara <ing.jeisontamara@gmail.com>
2023-08-29 11:50:03 -0500

    aplicamos métodos request
    aplicamos métodos perfil y listar para mostrar diferentes datos en diferentes vistas

--- Workspace/spring-boot-web/src/main/java/com/practica/springboot/web/app/controllers/IndexController.java
+++ Workspace/spring-boot-web/src/main/java/com/practica/springboot/web/app/controllers/IndexController.java
@@ -1,15 +1,44 @@
 package com.practica.springboot.web.app.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.practica.springboot.web.app.models.Usuarios;
 
 @Controller
+@RequestMapping("/app")//creamos una ruta de primer nivel para diferenciar los metodos de la paginas principales
 public class IndexController {
 	
-	@GetMapping(value={"/index","/home","/"})
-	public String index(Model modelo) {
-		modelo.addAttribute("Titulo", "Hola springboot framework");
+	@GetMapping({"/index","/home","/"})//
+	public String index(Model modelo) {//declaramos la interface model para usar sus metodos 
+		modelo.addAttribute("titulo", "Hola springboot framework");//con el metodo addAtribute agregamos valores al modelo  
 		return "index";
+	}
+	
+	@RequestMapping("/perfil")
+	public String perfil(Model modelo){
+		//creamos el modelo 
+		Usuarios usuario = new Usuarios();
+		usuario.setNombre("jeison");
+		usuario.setApellido("tamara");
+		usuario.setEmail("jeison.andres@email.com");
+		
+		modelo.addAttribute("usuario", usuario);//pasamos el modelo a la vista
+		modelo.addAttribute("titulo", "Perfil de usuario: ".concat(usuario.getNombre()));
+		return "perfil";
+	}
+	
+	@RequestMapping("/listar")
+	public String listar(Model modelo){
+		List<Usuarios> usuarios = new ArrayList<>();
+		modelo.addAttribute("titulo", "Lista de usuarios");
+		modelo.addAttribute("usuarios", usuarios);
+		
+		return "listar";
 	}
 }
---
+++ Workspace/spring-boot-web/src/main/java/com/practica/springboot/web/app/models/Usuarios.java
@@ -0,0 +1,28 @@
+package com.practica.springboot.web.app.models;
+
+public class Usuarios {
+	private String nombre;
+	private String apellido;
+	private String email;
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+}
