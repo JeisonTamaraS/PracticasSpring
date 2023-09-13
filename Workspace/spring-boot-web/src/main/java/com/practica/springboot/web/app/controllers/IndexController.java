d8fc1e5a1a4640b71681d256134e1f64918d1b9a
jeiTamara <ing.jeisontamara@gmail.com>
2023-09-13 15:38:55 -0500

    Anotación @Value
    agregamos recursos estáticos y estilos css; inyectamos valores usando la anotación @Value y la configuración de application.properties

--- Workspace/spring-boot-web/src/main/java/com/practica/springboot/web/app/controllers/IndexController.java
+++ Workspace/spring-boot-web/src/main/java/com/practica/springboot/web/app/controllers/IndexController.java
@@ -3,6 +3,7 @@ package com.practica.springboot.web.app.controllers;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,9 +15,16 @@ import com.practica.springboot.web.app.models.Usuarios;
 @RequestMapping("/app")//creamos una ruta de primer nivel para diferenciar los metodos de la paginas principales
 public class IndexController {
 	
+	@Value("${texto.indexcontroller.index.titulo}")
+	private String textoIndex;
+	@Value("${texto.indexcontroller.perfil.titulo}")
+	private String textoPerfil;
+	@Value("${texto.indexcontroller.listar.titulo}")
+	private String textoListar;
+	
 	@GetMapping({"/index","/home","/"})//
 	public String index(Model modelo) {//declaramos la interface model para usar sus metodos 
-		modelo.addAttribute("titulo", "Hola springboot framework");//con el metodo addAtribute agregamos valores al modelo  
+		modelo.addAttribute("titulo", textoIndex);//con el metodo addAtribute agregamos valores al modelo  
 		return "index";
 	}
 	
@@ -29,7 +37,7 @@ public class IndexController {
 		usuario.setEmail("jeison.andres@email.com");
 		
 		modelo.addAttribute("usuario", usuario);//pasamos el modelo a la vista
-		modelo.addAttribute("titulo", "Perfil de usuario: ".concat(usuario.getNombre()));
+		modelo.addAttribute("titulo", textoPerfil.concat(usuario.getNombre()));
 		return "perfil";
 	}
 	
@@ -40,7 +48,7 @@ public class IndexController {
 		usuarios.add(new Usuarios("andres","sangregorio","andres2@email.com"));
 		usuarios.add(new Usuarios("luis","gregorio","luis@email.com"));
 		
-		modelo.addAttribute("titulo", "Lista de usuarios");
+		modelo.addAttribute("titulo", textoListar);
 		modelo.addAttribute("usuarios", usuarios);
 		
 		return "listar";
