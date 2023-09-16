517d6fe90f935cec9acf2f4c9e1448ff9e823897
jeiTamara <ing.jeisontamara@gmail.com>
2023-09-16 13:15:13 -0500

    Inyección de de denpencias
    Creamos un nuevo proyecto para implementar la inyección de dependencias añadiendo una clase servicios

---
+++ Workspace/spring-boot-di/src/main/java/com/practica/springboot/di/app/models/services/MiServicio.java
@@ -0,0 +1,11 @@
+package com.practica.springboot.di.app.models.services;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class MiServicio {
+
+		public String operacion() {
+			return "Alguna operacion importante...";
+		}
+}
