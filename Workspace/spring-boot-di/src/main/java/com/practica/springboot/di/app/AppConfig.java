7d5e2d62cc823fb775394fc36bb6f41bba21a6bc
jeiTamara <ing.jeisontamara@gmail.com>
2023-09-18 16:57:26 -0500

    Ejemplo factura
    Implementamos un ejemplo de y relaciones entre componentes; creamos clases para los componentes(Cliente, Producto, Facturas); inyectamos dependencias desde el application.properties.

--- Workspace/spring-boot-di/src/main/java/com/practica/springboot/di/app/AppConfig.java
+++ Workspace/spring-boot-di/src/main/java/com/practica/springboot/di/app/AppConfig.java
@@ -1,9 +1,15 @@
 package com.practica.springboot.di.app;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 
+import com.practica.springboot.di.app.models.domain.ItemFactura;
+import com.practica.springboot.di.app.models.domain.Producto;
 import com.practica.springboot.di.app.models.services.IServicio;
 import com.practica.springboot.di.app.models.services.MiServicio;
 
@@ -20,5 +26,16 @@ public class AppConfig {
 	public IServicio registrarMyServicio2(){
 		
 		return new MiServicio();
+	}
+	
+	@Bean("itemFactura")
+	public List<ItemFactura> registrarItems(){
+		Producto producto1= new Producto("Camara Sony", 100); 
+		Producto producto2= new Producto("Bicicleta saft aro 27", 200); 
+		
+		ItemFactura linea1 = new ItemFactura(producto1, 2);
+		ItemFactura linea2 = new ItemFactura(producto2, 4);
+		
+		return Arrays.asList(linea1, linea2);
 	}
 }
---
+++ Workspace/spring-boot-di/src/main/java/com/practica/springboot/di/app/models/domain/Factura.java
@@ -0,0 +1,45 @@
+package com.practica.springboot.di.app.models.domain;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Factura {
+
+	@Value("${factura.descripcion}")
+	private String descripcion;
+	
+	@Autowired
+	private Cliente cliente;
+	
+	@Autowired
+	private List<ItemFactura> item;
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	public List<ItemFactura> getItem() {
+		return item;
+	}
+
+	public void setItem(List<ItemFactura> item) {
+		this.item = item;
+	}
+
+}
